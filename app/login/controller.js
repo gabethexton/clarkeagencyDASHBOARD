35b1c831cc91b8eb0f9ff28612def87fa157d5ff
gabethexton <gabethexton@gmail.com>
2016-09-26 15:23:04 -0600

    tweaking login process

--- app/login/controller.js
+++ app/login/controller.js
@@ -12,18 +12,21 @@ app.controller('controller',['$http','$window',function($http, $window){
       console.log(response);
       $window.sessionStorage.token = response.data.token;
       vm.message = "Logged in successful";
+      console.log(vm.message);
 
     })
     .catch(function(err){
       console.log(err);
       delete $window.sessionStorage.token;
       vm.message = "Log in unsuccessful";
+      console.log(vm.message);
     });
 };
 
   vm.logout = function(){
     delete $window.sessionStorage.token;
     vm.message = "Log out successful";
+    console.log(vm.message);
 };
 
 }]);
